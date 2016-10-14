80f39afb35f123d27bac52ba6af4f9c126b4591a
Diede Rusticus <diede.rusticus@nubisonline.nl>
2016-10-14 16:49:03 +0200

    added all permutations

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 This project is for the course Knowledge Representation on the UvA.
 
+- paramaterisation of inflow and volume
+- we use VCs to infer further values
+- we infer, using the dependencies (I+, I-), the derivatives and construct all childnodes
+- proportionalities are used to get more information about the derivatives
+- these left overs are the states (actual children of parent)
+
 Collaborators: Nedko Savov, Diede Rusticus
--- main.py
+++ main.py
@@ -2,3 +2,42 @@
 # QR project
 # Nedko en Diede
 # 14/10/16
+
+#class Quantity():
+
+#class State(params):
+#    def __init__(self, name):
+#        inflow_q = params  
+#                          
+#class Influence():
+#    def __init__(self, name):
+
+# GLOBAL VARIABLES
+zp = ['zero','plus']
+zpm = ['zero','plus','max']
+derivs = ['-','0','+']
+        
+def set_initial_states():
+    all_states = []
+    #getting all permutations
+    for i in zp:
+        for j in zpm:
+            for k in zpm:
+                for l in derivs:
+                    for m in derivs:
+                        for n in derivs:
+                            all_states.append([i,l,j,m,k,n])
+                    
+    for state in all_states:
+        print state
+    
+    
+    
+                       
+#    state = State(params)
+#    if state.inflow_q == zpm[1]: # if inflow is plus
+#        volume_d = derivs[2] # the derivative of the volume is +
+   
+#START PROGRAM
+if __name__ == "__main__":
+    set_initial_states()
